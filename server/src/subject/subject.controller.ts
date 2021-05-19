0d94182b8c1159a4b0b90ebef4392c7789a337c1
Ncorvenius <nicorvenius@gmail.com>
2021-05-19 17:49:50 +0300

    Story (DIS-4): Add role validation for Subject

--- server/src/subject/subject.controller.ts
+++ server/src/subject/subject.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import {Body, Controller, Delete, Get, Param, Post, Put, UseGuards} from "@nestjs/common";
 import { SubjectService } from "./subject.service";
 import { CreateSubjectDto } from "./dto/create-subject.dto";
+import {Roles} from "../decorators/roles.decorator";
+import {RolesGuard} from "../auth/Guards/roles.guard";
 
 @Controller('/subject')
 export class SubjectController {
@@ -8,16 +10,22 @@ export class SubjectController {
     constructor(private readonly subjectService: SubjectService ) {}
 
     @Get()
+    @Roles('admin', 'teacher')
+    @UseGuards(RolesGuard)
     getAllSubjects (){
         return this.subjectService.getAllSubjects();
     }
 
     @Post()
+    @Roles('admin', 'teacher')
+    @UseGuards(RolesGuard)
     createSubject(@Body() dto: CreateSubjectDto) {
         return this.subjectService.createSubject(dto);
     }
 
     @Delete(':id')
+    @Roles('admin', 'teacher')
+    @UseGuards(RolesGuard)
     deleteCourse (@Param('id') id: string) {
         return this.subjectService.deleteSubject(id);
 
