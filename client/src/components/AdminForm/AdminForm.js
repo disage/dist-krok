e4a501c9b5ed5ec082d1f97f0d7846cb58907eb0
Cyril Schemelev <kirill.schemelev@nitka.com>
2021-05-20 14:01:26 +0300

    feat (DIS-26): Add Editor

--- client/src/components/AdminForm/AdminForm.js
+++ client/src/components/AdminForm/AdminForm.js
@@ -1,85 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useState, useEffect, useRef} from 'react';
+import {useDispatch} from 'react-redux';
 
 import './AdminForm.scss';
+import {Editor} from '@tinymce/tinymce-react';
 
 // const store = new CourseStore();
 
-const AdminForm = ({ editFormStatus, settings }) => {
-  const [item, setItem] = useState({});
+const AdminForm = ({editFormStatus, settings}) => {
+    const [item, setItem] = useState({});
 
-  useEffect(() => {
-    settings.fields.inputs.map((item) => {
-      // item.defaultValue && setItem({ [item.name]: item.defaultValue });
-      item.defaultValue &&
+    useEffect(() => {
+        settings.fields.inputs.map((item) => {
+            // item.defaultValue && setItem({ [item.name]: item.defaultValue });
+            item.defaultValue &&
+            setItem((prevState) => ({
+                ...prevState,
+                [item.name]: item.defaultValue,
+            }));
+            return 0;
+        });
+    }, []);
+
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
+
+    const dispatch = useDispatch();
+
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
         setItem((prevState) => ({
-          ...prevState,
-          [item.name]: item.defaultValue,
+            ...prevState,
+            [name]: value,
         }));
-      return 0;
-    });
-  }, []);
+    };
 
-  const dispatch = useDispatch();
-
-  const [error, setError] = useState('');
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setItem((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (method) => {
-    try {
-      setError('');
-      dispatch(method(item));
-      editFormStatus(false);
-    } catch {
-      setError('Failed to change data');
-    }
-  };
-  return (
-    <>
-      <div className="formHeader">
-        <h2>{settings.title}</h2>
-        <span
-          onClick={() => {
+    const handleSubmit = async (method) => {
+        try {
+            setError('');
+            dispatch(method(item));
             editFormStatus(false);
-          }}>
+        } catch {
+            setError('Failed to change data');
+        }
+    };
+    return (
+        <>
+            <div className="formHeader">
+                <h2>{settings.title}</h2>
+                <span
+                    onClick={() => {
+                        editFormStatus(false);
+                    }}>
           Назад
         </span>
-      </div>
-      <form className="adminForm" method="POST">
-        {settings?.fields?.inputs?.map((item) => (
-          <div className="inputWrapper" key={item.name}>
-            <label htmlFor={item.name}>{item.text}</label>
-            <input
-              type={item.type}
-              name={item.name}
-              onChange={handleChange}
-              defaultValue={item.defaultValue}
-            />
-          </div>
-        ))}
-        <div className="btnWrapper">
-          {settings?.fields?.buttons?.map((item) => (
-            <button
-              key={item.value}
-              className="adminFormBtn"
-              onClick={(event) => {
-                event.preventDefault();
-                handleSubmit(item.method);
-              }}>
-              {item.value}
-            </button>
-          ))}
-        </div>
-      </form>
-    </>
-  );
+            </div>
+            <form className="adminForm" method="POST">
+                {settings?.fields?.inputs?.map((item) => (
+                    <div className="inputWrapper" key={item.name}>
+                        <label htmlFor={item.name}>{item.text}</label>
+                        <input
+                            type={item.type}
+                            name={item.name}
+                            onChange={handleChange}
+                            defaultValue={item.defaultValue}
+                        />
+                    </div>
+                ))}
+                <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    initialValue="<p>This is the initial content of the editor.</p>"
+                    apiKey="2gu4ndvf4r33jvmnz0rkrvy7lrt26od89emjyo68oaqx3k9s"
+                    init={{
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                            'advlist autolink lists link image charmap print anchor',
+                            'searchreplace visualblocks code fullscreen',
+                            'insertdatetime media table paste code help wordcount',
+                        ],
+                        toolbar: 'undo redo | formatselect | ' +
+                            'bold italic backcolor | alignleft aligncenter ' +
+                            'alignright alignjustify | bullist numlist outdent indent | ' +
+                            'removeformat | help',
+                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                    }}
+                />
+                <button onClick={log}>Log editor content</button>
+                <div className="btnWrapper">
+                    {settings?.fields?.buttons?.map((item) => (
+                        <button
+                            key={item.value}
+                            className="adminFormBtn"
+                            onClick={(event) => {
+                                event.preventDefault();
+                                handleSubmit(item.method);
+                            }}>
+                            {item.value}
+                        </button>
+                    ))}
+                </div>
+            </form>
+        </>
+    );
 };
 
 export default AdminForm;
